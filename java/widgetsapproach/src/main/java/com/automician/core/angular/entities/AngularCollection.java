f0d83370fd7e0a24e3ab56b86306a472ea9c4001
Julia Iluhina <julia.v.iluhina@gmail.com>
2016-12-30 20:08:34 +0200

    AngularCollection refactoring

--- java/widgetsapproach/src/main/java/com/automician/core/angular/entities/AngularCollection.java
+++ java/widgetsapproach/src/main/java/com/automician/core/angular/entities/AngularCollection.java
@@ -2,76 +2,38 @@ package com.automician.core.angular.entities;
 
 import com.automician.core.angular.wait.AngularWait;
 import com.codeborne.selenide.*;
-import com.codeborne.selenide.ex.UIAssertionError;
 import com.codeborne.selenide.impl.*;
-import com.codeborne.selenide.logevents.SelenideLog;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 
 import java.util.*;
-
-import static com.codeborne.selenide.Condition.not;
-import static com.codeborne.selenide.Configuration.assertionMode;
-import static com.codeborne.selenide.Configuration.collectionsPollingInterval;
-import static com.codeborne.selenide.Configuration.collectionsTimeout;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
-import static com.codeborne.selenide.logevents.ErrorsCollector.validateAssertionMode;
-import static com.codeborne.selenide.logevents.LogEvent.EventStatus.PASS;
-import static java.util.stream.Collectors.toList;
-
 
 public class AngularCollection extends ElementsCollection {
 
     private ElementsCollection collection;
 
-    public AngularCollection(ElementsCollection collection) {
+    public AngularCollection(final ElementsCollection collection) {
         super(new WebElementsCollection() {
             @Override
             public List<WebElement> getActualElements() {
-                return null;
+                List<WebElement> elements = new ArrayList<WebElement>();
+                for (SelenideElement element : collection) {
+                    elements.add(element.getWrappedElement());
+                }
+                return elements;
             }
 
             @Override
             public String description() {
-                return this.toString();
+                return "AngularCollection#WebElementsCollection " + this.toString();
             }
         });
         this.collection = collection;
     }
 
     @Override
-    public ElementsCollection shouldHave(final CollectionCondition... conditions) {
-        AngularWait.waitForRequestsToFinish();
-        this.collection.shouldHave(conditions);
-        return this;
-    }
-
-    @Override
-    protected ElementsCollection should(String prefix, CollectionCondition... conditions) {
-        return this.shouldHave(conditions);
-    }
-
-    @Override
-    public ElementsCollection shouldBe(final CollectionCondition... conditions) {
-        return this.shouldHave(conditions);
-    }
-
-    @Override
-    public ElementsCollection shouldHaveSize(int expectedSize) {
-        return this.shouldHave(CollectionCondition.size(expectedSize));
-    }
-
-    @Override
     protected void waitUntil(CollectionCondition condition, long timeoutMs) {
-        long oldTimeOut = Configuration.collectionsTimeout;
-        try {
-            Configuration.collectionsTimeout = timeoutMs;
-            this.shouldHave(condition);
-        } finally {
-            Configuration.collectionsTimeout = oldTimeOut;
-        }
+        AngularWait.waitForRequestsToFinish();
+        super.waitUntil(condition, timeoutMs);
     }
 
     @Override
